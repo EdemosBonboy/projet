6a4f37937519ac6608f65f2fae91105cb5e7a78d
yovoedem33 <yovoedem33@gmail.com>
2017-10-13 18:27:42 -0400

    Create and deploy students page on heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  resources :students
+
+  get 'students/new'
+
+  get 'students/edit'
+
+  get 'students/delete'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
